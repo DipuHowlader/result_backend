14bd3e86181191ffbd2a313a65fb566734aa7a07
dipuhowlader <dipuhowlader33@gmail.com>
2022-07-11 00:56:19 +0600

    settings updated

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://aqueous-citadel-50825.herokuapp.com"]
 
 # CORS_ALLOWED_ORIGINS = [
 # "http://127.0.0.1:5500",
